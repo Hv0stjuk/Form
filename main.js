308b93ffb38eb099f6ffdf5531e56bede88dbd79
Hv0stjuk <wot.hvostjuk@gmail.com>
2021-04-25 19:50:52 +0300

    feat: add container

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ function switchNavigationItem() {
   const elementsNavigationList = document.querySelector('.nav__list');
   const navigationItemsArry = elementsNavigationList.querySelectorAll('.nav__link');
   elementsNavigationList.addEventListener ('click', ({target}) => {
-    if (target.className === 'nav__link') {
-      navigationItemsArry.forEach(el => el.classList.remove('nav__link--active'));
-      target.classList.add('nav__link--active');
-      console.log(target.className);
-    }
+    navigationItemsArry.forEach(el => el.classList.remove('nav__link--active'));
+    target.classList.toggle('nav__link--active');
   });
 };
 switchNavigationItem();
